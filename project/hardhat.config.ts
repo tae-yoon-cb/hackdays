a81fad0dd7b80d9ca8a1e3cb07807aae78daf34f
Mo Kudeki <mo.kudeki@coinbase.com>
2022-02-02 10:31:03 -0800

    Add scripts for hardhat helpers

--- project/hardhat.config.ts
+++ project/hardhat.config.ts
@@ -3,15 +3,25 @@ import * as dotenv from "dotenv";
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
 import "@typechain/hardhat";
 import "solidity-coverage";
 
-import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-web3";
-import 'hardhat-deploy';
-
+import "hardhat-deploy";
+import { getContract } from "./scripts/helpers";
 
 dotenv.config();
+
+task("mint", "Mints from the NFT contract")
+  .addParam("address", "The address to receive a token")
+  .setAction(async function (taskArguments, hre) {
+    const contract = await getContract("MoNFT", hre);
+    const transactionResponse = await contract.mintTo(taskArguments.address, {
+      gasLimit: 500_000,
+    });
+    console.log(`Transaction Hash: ${transactionResponse.hash}`);
+  });
 
 task("balance", "Prints an account's balance")
   .addParam("account", "The account's address")
@@ -25,14 +35,14 @@ task("balance", "Prints an account's balance")
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   namedAccounts: {
-    deployer: 0
+    deployer: 0,
   },
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts: {
-        mnemonic: process.env.MNEMONIC || ""
-      }
+        mnemonic: process.env.MNEMONIC || "",
+      },
     },
   },
   etherscan: {
